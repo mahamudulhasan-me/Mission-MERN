2945079bba2fc9b23c188a7a33c7577764909f1a
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-01-21 17:13:30 +0600

    start tailwind

---
+++ Milestone-03/Module-15/penguin-faishon/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FABE4C",
+        secondary: "#363958",
+        p: "#3E3E3E",
+      },
+    },
+  },
+  plugins: [],
+};
