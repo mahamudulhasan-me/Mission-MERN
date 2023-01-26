993c4053bd5154ca0b85acb0ed8796d395c2f5f1
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-01-26 21:35:23 +0600

    array concenpt clear

--- Milestone-04/Module-18/index.js
+++ Milestone-04/Module-18/index.js
@@ -1,9 +1,16 @@
 var numList = [70, 81, 53, 79, 20];
+// find element by index number
 var element = numList[0];
 console.log(element);
+// set new value in consisting index value
 numList[0] = 69;
 console.log(numList);
 var nextElement = numList[3];
 console.log(nextElement);
 numList[3] = 96;
 console.log(numList);
+
+// find index number by elemtn
+var findElement = numList.indexOf(69);
+console.log(findElement);
+console.log(numList.indexOf(53));
