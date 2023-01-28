7f4017621d2f7f489b72f43b61855c445de41d2e
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-01-28 15:41:09 +0600

    Moudle-19 start

---
+++ Milestone-04/Module-18/problem3.js
@@ -0,0 +1,12 @@
+var num1 = 13;
+var num2 = 79;
+var num3 = 45;
+
+if (num1 > num2 && num3) {
+  console.log("First Number is a largest Number");
+}
+if (num2 > num1 && num3) {
+  console.log("Second Number is a largest Number");
+} else {
+  console.log("Number three is a largest Number");
+}
---
+++ Milestone-04/Module-19/array-average.js
@@ -0,0 +1,7 @@
+// var numbers = [75.25, 65, 80, 35.45, 99.5];
+// var totalNum = 0;
+// for (var i = 0; i < numbers.length; i++) {
+//   totalNum += numbers[i];
+// }
+// var average = parseFloat((totalNum / numbers.length).toFixed(2));
+// console.log(average);
