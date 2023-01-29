b317f7c34ffbe378541d8db8fa0dba404f02d97e
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-01-29 21:44:08 +0600

    start module-20

---
+++ Milestone-04/Module-20/function.js
@@ -0,0 +1,7 @@
+// function declaration
+function bringSingara(taka) {
+  console.log("mama singra den!");
+  console.log("koto takar singra niben?");
+  console.log("amake " + taka + "r singra den");
+}
+bringSingara(80);
