0fc12687bb1cf694ea0d8eaaba42494a110f5a96
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-01-30 16:19:30 +0600

    add problem 6

---
+++ Milestone-04/Module-20/problem6.js
@@ -0,0 +1,9 @@
+// fullName নামে একটা ফাংশন তৈরী কর যেটা তোমার নামের প্রথম অংশ এবং তোমার নামের শেষের অংশ প্যারামিটার হিসেবে নিবে। আর তোমার নামের দুই অংশ জোড়া দিয়ে আউটপুট হিসেবে তোমার পূর্ন নাম রিটার্ন করে দিবে। যেমন ধরো, hablu এবং kanto ইনপুট প্যারামিটার হিসেবে দিলে আউটপুট হিসেবে hablu kanto রিটার্ন করবে।
+function fullName(fName, lName) {
+  let result = fName + " " + lName;
+  return result;
+}
+const fName = "Mahmudul";
+const lName = "Hasan";
+const showFullName = fullName(fName, lName);
+console.log(showFullName);
