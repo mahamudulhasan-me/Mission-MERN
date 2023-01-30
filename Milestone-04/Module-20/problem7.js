57ea9463f7a978fd2c48c54772a5a3f27d6efae9
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-01-30 16:21:53 +0600

    add problem 7

---
+++ Milestone-04/Module-20/problem7.js
@@ -0,0 +1,11 @@
+// একটা ফাংশন লিখবা যেটাকে তুমি কোন সংখ্যাকে ইনপুট হিসেবে দিলে সে সেই সংখ্যার square করে সেই square কে রিটার্ন করবে।
+
+// অর্থাৎ তুমি ইনপুট হিসেবে 5 দিলে সেটাতে স্কয়ারে হিসেবে 25 আউটপুট হিসেবে পাবে।
+
+function square(num) {
+  let square = num * num;
+  return square;
+}
+const myNum = 5;
+const result = square(myNum);
+console.log(result);
