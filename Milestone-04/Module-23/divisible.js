4c8454412d225801c25d1f59aa898bea1f73d9f3
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-02-02 22:14:00 +0600

    todays last commit

--- Milestone-04/Module-23/divisible.js
+++ Milestone-04/Module-23/divisible.js
@@ -1,9 +1,11 @@
 for (let i = 1; i <= 50; i++) {
-  if (i % 3 === 0) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log(i, "-", "foobar");
+  } else if (i % 3 == 0) {
     console.log(i, "-", "foo");
   } else if (i % 5 === 0) {
     console.log(i, "-", "bar");
-  } else if (i % 3 === 0 && i % 5 === 0) {
-    console.log(i, "-", "foobar");
+  } else {
+    console.log(i);
   }
 }
