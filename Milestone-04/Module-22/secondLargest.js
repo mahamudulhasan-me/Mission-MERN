21fe18483e43b4603f7813ee243d698c22c6b83c
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-02-05 22:02:07 +0600

    todays emni commit

--- Milestone-04/Module-22/secondLargest.js
+++ Milestone-04/Module-22/secondLargest.js
@@ -1,12 +1,29 @@
-// first way
+// // first way
 function secondLargest(yourArray) {
   let sortingArray = yourArray.sort(function (a, b) {
-    return b - a;
+    return a - b;
   });
   let secondLargest = sortingArray[1];
   return secondLargest;
 }
-// second way
+// // second way
+// function findSecLargest(array) {
+//   let largest = array[0];
+//   let secondLargest = array[1];
+//   if (largest < secondLargest) {
+//     [largest, secondLargest] = [secondLargest, largest];
+//   }
+//   for (let i = 2; i < array.length; i++) {
+//     if (largest < array[i]) {
+//       secondLargest = largest;
+//       largest = array[i];
+//     } else if (secondLargest < array[i]) {
+//       secondLargest = array[i];
+//     }
+//   }
+//   return secondLargest;
+// }
+
 function findSecLargest(array) {
   let largest = array[0];
   let secondLargest = array[1];
@@ -27,3 +44,4 @@ function findSecLargest(array) {
 const myArray = [10, 48, 5, 4, 64, 88, 79, 66, 44, 5, 47, 54, 99, 100];
 const result = findSecLargest(myArray);
 console.log(result);
+console.log("He is" + (2021 - 1999) + "years old");
