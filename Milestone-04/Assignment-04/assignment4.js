971edc42e4d3872af38cbfae4bf5a30260fbec58
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-02-07 16:29:49 +0600

    mybe final commit

--- Milestone-04/Assignment-04/assignment4.js
+++ Milestone-04/Assignment-04/assignment4.js
@@ -4,18 +4,18 @@ Problem 1: Let’s play a mind game
 This program will take a positive number as an argument and return the result after the arithmetic
 operation and return an error message if anything other than a positive number is given.
 ----------------------------------------------- */
-function mindGame(number) {
-  if (number >= 0 && typeof number === "number") {
+function mindGame(positiveNum) {
+  if (positiveNum >= 0 && typeof positiveNum === "number") {
     // arithmetic operation of a given number
-    return (number * 3 + 10) / 2 - 5;
+    return (positiveNum * 3 + 10) / 2 - 5;
   }
-  return "Oops! Please Give a Positive Number.";
+  return "Please Give a Positive Number.";
 }
 
 /* -----------------------------------------------
 Problem 2: Finding even or odd
 ------------------------------
-This program will take a string data as an argument and return "Even" if length of this string
+This function will take a string data as an argument and return "Even" if length of this string
 as even  and "Odd" if the length is odd. Any data other than string will return an error message.
 ----------------------------------------------- */
 function evenOdd(str) {
@@ -27,66 +27,74 @@ function evenOdd(str) {
     return "odd";
   }
   //If the input is not a string, it will return
-  return "Oops! Please Give a String Input.";
+  return "Please Give a String as Input.";
 }
 
 /* -----------------------------------------------
 Problem 3: Is Less or Greater than seven
 ----------------------------------------
-This program will take a number as an argument, if this number is less than seven after subtracting
+This function will take a number as an argument, if this number is less than seven after subtracting
 it from seven, it will return the subtraction result. Otherwise, it will return the double of the input.
 ----------------------------------------------- */
-function isLGSeven(num) {
-  if (typeof num === "number") {
-    let absoluteDiff = num - 7;
+function isLGSeven(number) {
+  if (typeof number === "number") {
+    let absoluteDiff = number - 7;
     if (absoluteDiff < 7) {
       return absoluteDiff;
     }
-    return num * 2;
+    return number * 2;
   }
   //If the input is not a number, it will return
-  return "Oops! Please Give Any Number.";
+  return "Please Give Any Valid Number.";
 }
 
 /* -----------------------------------------------
 Problem 4: Finding Bad data
 ---------------------------
-This program will take an array of numbers as an argument, it will return all the bad data in this array,
-i.e. the numbers below zero. Any input other than array will return an error message.
+This function will take an array of numbers as an argument, it will return all the bad data 
+in this array, i.e. the numbers below zero. Any input other than array will return an error message.
 ----------------------------------------------- */
 function findingBadData(numbersArray) {
   if (Array.isArray(numbersArray)) {
     let badDataCounter = 0;
     const arrayLen = numbersArray.length;
     for (let i = 0; i < arrayLen; i++) {
+      // Checking for bad data
       if (numbersArray[i] < 0) {
         badDataCounter++;
       }
     }
     return badDataCounter;
   }
-  return "Oops! Please Give An Array.";
+  //If the input is not an array, it will return
+  return "Please Give An Array.";
 }
 
 /* -----------------------------------------------
 Problem 5: Convert your gems into diamond
+-----------------------------------------
+This function will take the number of gems of three friends as argument and convert the gems to diamonds 
+by multiplying them by 21, 32 and 43 respectively. If the number of total diamonds is greater than 2000
+then 2000 will be subtracted from the total diamonds and the remaining diamonds will be returned.
 ----------------------------------------------- */
-function gemsToDiamond(firstFrndGems, secondFrndGems, thirdFrndGems) {
+function gemsToDiamond(firstFriendGems, secondFriendGems, thirdFriendGems) {
   if (
-    typeof firstFrndGems === "number" &&
-    typeof secondFrndGems === "number" &&
-    typeof thirdFrndGems === "number"
+    typeof firstFriendGems === "number" &&
+    typeof secondFriendGems === "number" &&
+    typeof thirdFriendGems === "number"
   ) {
-    const firstFrndDiamonds = 21 * firstFrndGems;
-    const secondFrndDiamonds = 32 * secondFrndGems;
-    const thirdFrndDiamonds = 43 * thirdFrndGems;
+    const firstFriendDiamonds = 21 * firstFriendGems;
+    const secondFriendDiamonds = 32 * secondFriendGems;
+    const thirdFriendDiamonds = 43 * thirdFriendGems;
     //add three friends diamonds
     let totalDiamonds =
-      firstFrndDiamonds + secondFrndDiamonds + thirdFrndDiamonds;
-    if (totalDiamonds > 1000 * 2) {
+      firstFriendDiamonds + secondFriendDiamonds + thirdFriendDiamonds;
+    // Checking for greater than 2000
+    if (totalDiamonds >= 1000 * 2) {
       return totalDiamonds - 2000;
     }
     return totalDiamonds;
   }
-  return "Oops! Please Give a Valid Three Number.";
+  //If the three input is not a number, it will return
+  return "Please Give a Valid Three Number.";
 }
