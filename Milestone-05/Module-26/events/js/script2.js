5998ba51c10baf26cc368809ff483a566d767496
mahamudulhasan-me <mahamudulhasan.org@gmail.com>
2023-02-10 00:51:31 +0600

    midnight commit

---
+++ Milestone-05/Module-26/events/js/script2.js
@@ -0,0 +1,8 @@
+document.getElementById("commentSubmit").addEventListener("click", function () {
+  console.log("it work");
+});
+// --------------------------------------
+
+// submitButton.addEventListener("click", function () {
+//   console.log("it work");
+// });
