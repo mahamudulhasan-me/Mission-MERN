069b22082c04dec0142be789ff39577f64baf66c
mahamudulhasan-me <mahamudulhasan.org@gmail.com>
2023-02-15 11:56:00 +0600

    shopping cart done

---
+++ Milestone-05/Module-29/shopingCart/js/common.js
@@ -0,0 +1,31 @@
+function quantityCount(isPositive, quantityId) {
+  const quantity = document.getElementById(quantityId);
+  const quantityToNum = parseInt(quantity.value);
+  let productQuantity;
+  if (isPositive) {
+    quantity.value = quantityToNum + 1;
+    productQuantity = quantity.value;
+  } else {
+    quantity.value = quantityToNum - 1;
+    productQuantity = quantity.value;
+  }
+  return productQuantity;
+}
+function setInnerText(id, price) {
+  document.getElementById(id).innerText = price;
+}
+function getTotalPrice(productId) {
+  const total = document.getElementById(productId);
+  const totalToNum = parseInt(total.innerText);
+  return totalToNum;
+}
+function subtotal() {
+  const mobileTotal = getTotalPrice("phone_price");
+  const caseTotal = getTotalPrice("case_price");
+  const totalPrice = mobileTotal + caseTotal;
+  setInnerText("subtotal", totalPrice);
+  const taxAmount = parseFloat((totalPrice * 0.1).toFixed(2));
+  setInnerText("tax", taxAmount);
+  const totalAmount = totalPrice + taxAmount;
+  setInnerText("total", totalAmount);
+}
