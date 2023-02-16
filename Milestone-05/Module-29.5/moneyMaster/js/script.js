d88e3e9205d5406f70a17dd01e0cafa014d28b49
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-02-16 15:52:53 +0600

    money master done

---
+++ Milestone-05/Module-29.5/moneyMaster/js/script.js
@@ -0,0 +1,52 @@
+function getValue(id) {
+  const items = document.getElementById(id);
+  const itemsValueToNum = parseFloat(items.value);
+  return itemsValueToNum;
+}
+document
+  .getElementById("btn_calculate")
+  .addEventListener("click", function (e) {
+    const food = getValue("food");
+    const rent = getValue("rent");
+    const clothes = getValue("clothes");
+    const income = getValue("income");
+    const totalExpenses = food + rent + clothes;
+    if (totalExpenses > income) {
+      alert("Sorry, you don't have enough money");
+    } else {
+      if (isNaN(totalExpenses)) {
+        alert("Please enter a number");
+      } else {
+        //   calculate total expenses
+        document.getElementById("total_expenses").innerText = totalExpenses;
+
+        //   balance after total expenses subtract from income
+        const balanceAfterExpenses =
+          parseFloat(income) - parseFloat(totalExpenses);
+        document.getElementById("balance").innerText = balanceAfterExpenses;
+      }
+    }
+  });
+document.getElementById("save").addEventListener("click", function () {
+  const income = getValue("income");
+  const savingRate = getValue("saving_rate");
+  const savingAmount = (savingRate * income) / 100;
+  if (isNaN(savingAmount)) {
+    alert("Please enter a Saving Rate");
+  } else {
+    document.getElementById("saving_amount").innerText = savingAmount;
+
+    const balanceAfterExpenses = document.getElementById("balance").innerText;
+    document.getElementById("remaining_balance").innerText =
+      balanceAfterExpenses - savingAmount;
+  }
+});
+let allInput = document.querySelectorAll('input[type="number"]');
+for (let input of allInput) {
+  input.addEventListener("keyup", function (e) {
+    if (e.target.value < 0) {
+      alert("Please enter a number");
+      e.target.value = "";
+    }
+  });
+}
