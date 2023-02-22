043b758d8c0c24f1a94c52f3eb72a51021996ed0
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-02-22 13:44:29 +0600

    32.4 problem4

---
+++ Milestone-06/Module-32.5/problem4.js
@@ -0,0 +1,32 @@
+// 4) How will you display
+// a) Petersburg
+// b) Herry
+let students = {
+  2222: {
+    name: "Jack",
+    section: "C",
+    class: "IX",
+    address: {
+      "building no": 12,
+      street: "St. Jonson",
+      city: "Petersburg",
+      country: "UK",
+    },
+  },
+  3333: {
+    name: "Herry",
+    section: "D",
+    class: "X",
+    address: {
+      "building no": 85,
+      street: "DC road",
+      city: "Kachukhet",
+      country: "Bangladesh",
+    },
+  },
+};
+const petursburg = students[2222].address.city;
+const herry = students[3333].name;
+
+console.log(petursburg);
+console.log(herry);
