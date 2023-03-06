af3badbf919eda2b2ec3f1617b6737ee9245692c
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-03-06 13:30:44 +0600

    quiz done

---
+++ Milestone-07/Module-40/quiz.js
@@ -0,0 +1,19 @@
+// const a = "mahmudul";
+// console.log(a.toUpperCase());
+// // console.log((a.toUpperCase = () => "JOHN"));
+// const name = "John";
+// // name.toUpperCase = () => "JOHN";
+// console.log(name.toUpperCase());
+const arr = [1, 2, 3];
+const a = arr.slice(-2, 1);
+console.log(a);
+const greeting = "Hello, world!";
+console.log(greeting);
+// console.log(x);
+// functionss();
+// console.log(5 + a);
+// const myObj = {
+//   a: "a",
+// };
+// console.log(myObj.b);
+console.log(tumi());
