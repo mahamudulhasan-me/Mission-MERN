7535a07c8a70092755bd53ee992376782d6f0f97
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-03-13 17:59:45 +0600

    add target-2 paray sesh

---
+++ Milestone-07/Module-44/target-2.js
@@ -0,0 +1,6 @@
+const myStr = "This is a string";
+// console.log(myStr.charAt(5)); // This is a string);
+// for (let i = 0; i < myStr.length; i++) {
+//   console.log(myStr.charAt(i));
+// }
+console.log(myStr.toUpperCase());
