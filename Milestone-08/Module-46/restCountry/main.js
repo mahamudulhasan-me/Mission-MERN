959664aebad322c4692d85425b02dfcdbedcc0df
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-03-20 16:58:55 +0600

    destructruing in different way

--- Milestone-08/Module-46/restCountry/main.js
+++ Milestone-08/Module-46/restCountry/main.js
@@ -7,20 +7,34 @@ const loadAllCountry = () => {
     });
 };
 loadAllCountry();
-
 const showAllCounty = (country) => {
-  const container = document.getElementById("root");
-  const showCase = ({ name, capital, flags, languages }) => {
-    // const { name, capital, flags } = country;
-
-    container.innerHTML += `
-      <div class="single-country">
+  const singleCountry = country.map((country) => showCase(country));
+  let container = document.getElementById("root");
+  container.innerHTML += singleCountry.join("");
+  //   console.log(singleCountry);
+};
+const showCase = ({ name, flags, capital }) => {
+  return `<div class="single-country">
         <h2>${name.common}</h2>
         <h3>Capital: ${capital}</h3>
-        <p>Language: ${languages["eng"]}</p>
+
         <img src="${flags.png}" alt="" />
-      </div>
-      `;
-  };
-  country.slice(0, 9).map((country) => showCase(country));
+    </div>`;
 };
+
+// const showAllCounty = (country) => {
+//   const container = document.getElementById("root");
+//   const showCase = ({ name, capital, flags, languages }) => {
+//     // const { name, capital, flags } = country;
+
+//     container.innerHTML += `
+//       <div class="single-country">
+//         <h2>${name.common}</h2>
+//         <h3>Capital: ${capital}</h3>
+//         <p>Language: ${languages["eng"]}</p>
+//         <img src="${flags.png}" alt="" />
+//       </div>
+//       `;
+//   };
+//   country.slice(0, 9).map((country) => showCase(country));
+// };
