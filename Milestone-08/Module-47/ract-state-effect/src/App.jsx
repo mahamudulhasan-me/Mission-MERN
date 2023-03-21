d77c3e3c6ac7dea897f4f86843830f347d993b77
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-03-21 16:18:28 +0600

    counter by react

--- Milestone-08/Module-47/ract-state-effect/src/App.jsx
+++ Milestone-08/Module-47/ract-state-effect/src/App.jsx
@@ -1,27 +1,45 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const products = [
-    { name: "Laptop", price: 45000 },
-    { name: "Laptop", price: 45000 },
-    { name: "Laptop", price: 45000 },
-    { name: "Laptop", price: 45000 },
-  ];
   return (
     <div className="App">
-      {products.map((product) => (
-        <Product name={product.name} price={product.price}></Product>
-      ))}
+      <Counter></Counter>
     </div>
   );
 }
-function Product(props) {
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  const countIncrement = () => setCount(count + 1);
+  const countDecrease = () => setCount(count - 1);
   return (
-    <div className="product">
-      <h2>Name: {props.name}</h2>
-      <p>Price {props.price}</p>
+    <div className="Counter">
+      <h2>Counter: {count}</h2>
+      <button onClick={countIncrement}>Increase</button>
+      <button onClick={countDecrease}>Decrease</button>
     </div>
   );
 }
+
+// for dynamic data
+// {products.map((product) => (
+//   <Product name={product.name} price={product.price}></Product>
+// ))}
+// const products = [
+//   { name: "Laptop", price: 45000 },
+//   { name: "Laptop", price: 45000 },
+//   { name: "Laptop", price: 45000 },
+//   { name: "Laptop", price: 45000 },
+// ];
+
+// function Product(props) {
+//   return (
+//     <div className="product">
+//       <h2>Name: {props.name}</h2>
+//       <p>Price {props.price}</p>
+//     </div>
+//   );
+// }
 
 export default App;
