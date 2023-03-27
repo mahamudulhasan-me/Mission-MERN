a5ace54a64227d0fb8ab978f18da9a3b3dda4ac4
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-03-27 14:38:48 +0600

    ema john add to cart complete

--- Milestone-08/Module-50/ema-john/src/components/Product/Product.jsx
+++ Milestone-08/Module-50/ema-john/src/components/Product/Product.jsx
@@ -3,6 +3,7 @@ import cart from "../../assets/images/trolley.png";
 
 const Product = (props) => {
   const { name, price, img, seller, ratings } = props.product;
+  const addToCart = props.addToCart;
   return (
     <div className="border rounded-lg relative">
       <div className="p-2">
@@ -16,7 +17,10 @@ const Product = (props) => {
           </div>
         </div>
       </div>
-      <div className="bg-[#FFE0B3] flex justify-center items-center gap-3 font-semibold border-t rounded-b-lg">
+      <div
+        onClick={() => addToCart(props.product)}
+        className=" cursor-pointer bg-[#FFE0B3] flex justify-center items-center gap-3 font-semibold border-t rounded-b-lg"
+      >
         <p>Add to Cart</p>
         <img src={cart} alt="" className="w-10 py-2 " />
       </div>
--- Milestone-08/Module-50/ema-john/src/components/ProductContainer/ProductContainer.jsx
+++ Milestone-08/Module-50/ema-john/src/components/ProductContainer/ProductContainer.jsx
@@ -2,31 +2,44 @@ import { faArrowRight, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useEffect, useState } from "react";
 import Product from "../Product/Product";
+
 const ProductContainer = () => {
-  const [product, setProduct] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
+  const addToCart = (product) => {
+    const newCart = [...cart, product];
+    setCart(newCart);
+  };
+  let totalPrice = cart.reduce((pre, current) => pre + current.price, 0);
+  let shippingCharge = 100;
+
   useEffect(() => {
     fetch("products.json")
       .then((response) => response.json())
-      .then((products) => setProduct(products));
+      .then((products) => setProducts(products));
   }, []);
   return (
     <div className="grid grid-cols-12 pl-[5%] gap-8">
       <div className="col-span-9">
         <div className="grid grid-cols-3 gap-5 mt-28">
-          {product.map((product) => (
-            <Product product={product} key={product.id}></Product>
+          {products.map((product) => (
+            <Product
+              product={product}
+              key={product.id}
+              addToCart={addToCart}
+            ></Product>
           ))}
         </div>
       </div>
-      <div className="bg-[#FFE0B3] col-span-3 px-5 pt-8">
+      <div className="bg-[#FFE0B3] col-span-3 px-5 pt-8 sticky">
         <h2 className="text-center font-semibold text-2xl mb-5">
           Order Summary
         </h2>
         <div className="space-y-3">
-          <p>Selected Items: </p>
-          <p>Total Price: $ </p>
-          <p>Total Shipping Charge: $</p>
-          <p>Tax: $</p>
+          <p>Selected Items: {cart.length}</p>
+          <p>Total Price: $ {totalPrice}</p>
+          <p>Total Shipping Charge: ${shippingCharge}</p>
+          <p>Tax: ${(totalPrice + shippingCharge) * 0.01}</p>
           <h3>Grand Total</h3>
         </div>
         <div className="flex flex-col space-y-4 mt-5">
