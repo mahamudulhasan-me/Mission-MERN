819be02535b69e4d1a4f43fa47ed61edf819444a
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-03-27 14:08:28 +0600

    add product card

--- Milestone-08/Module-50/ema-john/src/App.jsx
+++ Milestone-08/Module-50/ema-john/src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header/Header";
+import ProductContainer from "./components/ProductContainer/ProductContainer";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <ProductContainer />
     </div>
   );
 }
--- Milestone-08/Module-50/ema-john/src/components/ProductContainer/ProductContainer.jsx
+++ Milestone-08/Module-50/ema-john/src/components/ProductContainer/ProductContainer.jsx
@@ -1,7 +1,45 @@
-import React from "react";
-
+import { faArrowRight, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useEffect, useState } from "react";
+import Product from "../Product/Product";
 const ProductContainer = () => {
-  return <div></div>;
+  const [product, setProduct] = useState([]);
+  useEffect(() => {
+    fetch("products.json")
+      .then((response) => response.json())
+      .then((products) => setProduct(products));
+  }, []);
+  return (
+    <div className="grid grid-cols-12 pl-[5%] gap-8">
+      <div className="col-span-9">
+        <div className="grid grid-cols-3 gap-5 mt-28">
+          {product.map((product) => (
+            <Product product={product} key={product.id}></Product>
+          ))}
+        </div>
+      </div>
+      <div className="bg-[#FFE0B3] col-span-3 px-5 pt-8">
+        <h2 className="text-center font-semibold text-2xl mb-5">
+          Order Summary
+        </h2>
+        <div className="space-y-3">
+          <p>Selected Items: </p>
+          <p>Total Price: $ </p>
+          <p>Total Shipping Charge: $</p>
+          <p>Tax: $</p>
+          <h3>Grand Total</h3>
+        </div>
+        <div className="flex flex-col space-y-4 mt-5">
+          <button className=" bg-rose-500 text-white text-lg  py-2">
+            Clear Cart <FontAwesomeIcon icon={faTrash} />
+          </button>
+          <button className=" bg-orange-500 text-white text-lg py-2">
+            Review Order <FontAwesomeIcon icon={faArrowRight} />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductContainer;
