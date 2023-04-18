fdb046767ce70022eb6cc5420465f039621302a3
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-04-18 18:39:19 +0600

    add dragon news

---
+++ Milestone-10/Module-62/the-dragon-news/src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDNPYugDj72-GPRx8VQyvfvMoxWLhoMwdc",
+  authDomain: "the-dragon-news-807d9.firebaseapp.com",
+  projectId: "the-dragon-news-807d9",
+  storageBucket: "the-dragon-news-807d9.appspot.com",
+  messagingSenderId: "1077428843315",
+  appId: "1:1077428843315:web:6c99c4eaa31eb4613ac446",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
