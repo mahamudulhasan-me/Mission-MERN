1608d1aacf281a3d8a9131c67eb71f37a4f0c44d
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-04-26 19:45:51 +0600

    filter news by category and id

--- Milestone-10/dragon-news-server/index.js
+++ Milestone-10/dragon-news-server/index.js
@@ -1,13 +1,39 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 const categories = require("./data/categories.json");
+const allNews = require("./data/news.json");
+
+app.use(cors());
+
 app.get("/", (req, res) => {
   res.send("Dragon server is running");
 });
 app.get("/categories", (req, res) => {
   res.send(categories);
+});
+// get all news
+app.get("/news", (req, res) => {
+  res.send(allNews);
+});
+// news find by id
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const newsById = allNews.find((news) => news._id === id);
+  res.send(newsById);
+});
+// news filter by id
+app.get("/categories/:id", (req, res) => {
+  const id = req.params.id;
+  if (id === "0") {
+    res.send(allNews);
+  } else {
+    const categoryNews = allNews.filter((news) => news.category_id === id);
+    res.send(categoryNews);
+  }
 });
 app.listen(port, () => {
   console.log(`Dragon server is running on ${port}`);
