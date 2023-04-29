d05a702261f524b07442749fb4128ea4c97ebd42
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-04-29 20:57:12 +0600

    ready to project for travel guru

---
+++ Milestone-10/travel-guru/src/layouts/Main.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Main = () => {
+  return <div>welcome to travel guru</div>;
+};
+
+export default Main;
---
+++ Milestone-10/travel-guru/src/routers/Routes.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layouts/Main";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+  },
+]);
+
+export default router;
