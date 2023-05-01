5f1093f11fbe68c89b56cd1e95f49118782a11ec
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-05-01 15:34:38 +0600

    add hotel info

--- Milestone-10/Travel Guru/travel-guru-server/index.js
+++ Milestone-10/Travel Guru/travel-guru-server/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+
 const port = process.env.PORT || 5000;
 
 const placeName = require("./data/travelPlace.json");
-
+const hotelRoomInfo = require("./data/hotelInfo.json");
 app.use(cors());
 
 app.get("/", (req, res) => {
@@ -17,6 +18,11 @@ app.get("/booking/:id", (req, res) => {
   const id = req.params.id;
   const placeById = placeName.find((place) => place.id == id);
   res.send(placeById);
+});
+
+// get hotel room info
+app.get("/hotel", (req, res) => {
+  res.send(hotelRoomInfo);
 });
 app.listen(port, () => {
   console.log("Travel Guru Listening on port " + port);
