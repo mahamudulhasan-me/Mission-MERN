cca7969c10f3a822b01f9e8a889fd57e5bc7f113
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-05-06 22:18:36 +0600

    connect with database successfully

--- Milestone-11/simple CRUD operation/simple-crud-server/index.js
+++ Milestone-11/simple CRUD operation/simple-crud-server/index.js
@@ -28,6 +28,17 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    // const database = client.db("usersDB");
+    // const userCollection = database.collection("users");
+    const db = client.db("usersDB");
+    const userCollection = db.collection("users");
+
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+      const result = await userCollection.insertOne(newUser);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
