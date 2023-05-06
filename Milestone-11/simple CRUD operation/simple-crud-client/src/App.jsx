4816b9d97a834446b8fa11011b0f222eda0365d3
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-05-06 22:19:37 +0600

    mongodb connect successfully

--- Milestone-11/simple CRUD operation/simple-crud-client/src/App.jsx
+++ Milestone-11/simple CRUD operation/simple-crud-client/src/App.jsx
@@ -1,9 +1,35 @@
 import "./App.css";
 
 function App() {
+  const handleUsers = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email };
+    console.log(user);
+    fetch(`http://localhost:8080/users`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((user) => {
+        console.log(user);
+      });
+    form.reset();
+  };
   return (
     <>
       <h2>Simple CRUD Operation</h2>
+      <hr />
+      <form onSubmit={handleUsers}>
+        <input type="text" name="name" placeholder="Name" /> <br />
+        <input type="email" name="email" placeholder="Email" /> <br />
+        <button>Add User</button>
+      </form>
     </>
   );
 }
