ae7d4dbb33c4f2e693a2ece6d4ad3461aad2f00f
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-05-17 18:37:56 +0600

    configure jwt operation

--- Milestone-11/Job Hunter/jobHunter-server/index.js
+++ Milestone-11/Job Hunter/jobHunter-server/index.js
@@ -1,13 +1,35 @@
 const express = require("express");
 const cors = require("cors");
-const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
+
+const app = express();
 const port = process.env.PORT || 4040;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
+const verifyJwt = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "Authorization required" });
+  }
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.JWT_SIGNATURE, (err, decode) => {
+    if (err) {
+      return res
+        .status(403)
+        .send({ error: true, message: "Authorization failed" });
+    } else {
+      req.decoded = decode;
+      next();
+    }
+  });
+};
 // ----------------------------------------------------
 app.get("/", (req, res) => {
   res.send("Job Hunter Running now");
@@ -15,7 +37,6 @@ app.get("/", (req, res) => {
 
 // =========================================================
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@cluster0.beeiwwt.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -36,12 +57,22 @@ async function run() {
     // create job post collection
     const jobPostCollection = jobHunterDB.collection("jobPost");
 
-    // ===========================================================
+    //===========================================================
     // create index
     const indexKey = { title: 1, category: 1 };
     const indexOption = { name: "titleCategory" };
     jobPostCollection.createIndex(indexKey, indexOption);
 
+    //===========================================================
+    // create JWT token
+    app.post("/jwt", (req, res) => {
+      const userID = req.body;
+      const token = jwt.sign(userID, process.env.JWT_SIGNATURE, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
+    //===========================================================
     // add job post on db
     app.post("/jobPost", async (req, res) => {
       req.body.createdAt = new Date();
@@ -69,9 +100,30 @@ async function run() {
         res.send(result);
       }
     });
-    app.get("/myjobs/:userId", async (req, res) => {
+    // get jobs by user
+    app.get("/myjobs/:userId", verifyJwt, async (req, res) => {
       const userId = req?.params?.userId;
+      const decodedInfo = req.decoded;
+
+      // if (decodedInfo.userId !== userId) {
+      //   return res
+      //     .status(403)
+      //     .send({ error: true, message: "Authorized Access" });
+      // }
       const query = { userID: userId };
+      const result = await jobPostCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    // get jobs by search
+    app.get("/myjobsBySearch/:search", async (req, res) => {
+      const searchText = req?.params?.search;
+      const query = {
+        $or: [
+          { title: { $regex: searchText, $options: "i" } },
+          { category: { $regex: searchText, $options: "i" } },
+        ],
+      };
       const result = await jobPostCollection.find(query).toArray();
       res.send(result);
     });
